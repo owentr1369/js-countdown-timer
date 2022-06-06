4c6bf56b4559beee13c1e414aba237fa3875b7e9
owentr1369 <tamvaa1306@gmail.com>
2022-06-06 10:14:34 +0700

    functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "1 Jan 2023";
+
+function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor((totalSeconds / 3600) % 24);
+  const minutes = Math.floor((totalSeconds / 60) % 60);
+  const seconds = Math.floor(totalSeconds % 60);
+
+  console.log(days, hours, minutes, seconds);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
+}
+
+setInterval(countdown, 1000);
